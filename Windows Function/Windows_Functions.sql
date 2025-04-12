644d7bfb6d9527619ff502c404c283b78ffcdb5b
Matias-Bustamante <matybustamante151@gmail.com>
2025-04-12 10:30:08 -0300

    Ejercicio Windows Function

---
+++ Windows Function/Windows_Functions.sql
@@ -0,0 +1,9 @@
+
+
+select
+c.category_name as name, 
+p.product_name as product_name, 
+p.unit_price as unite_price, 
+AVG(p.unit_price) over(partition by c.category_id) as avg_unit_price
+from categories c inner join products p 
+on c.category_id =p.category_id
